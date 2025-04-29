626a36cfcf5121198a46e052ca85f7c3d45ce7b2
hkhushi1209 <hkhushi1209@gmail.com>
2025-04-29 17:33:50 +0530

    Commit 1

---
+++ Module 1/Activity 1.py
@@ -0,0 +1,13 @@
+print("Welcome to Python Programming")
+name= "Khushi"
+print("My name is", name)
+age=12
+print("I'm",age,"years old")
+#Print a Number
+print(4)
+#Next line after print
+print("Hello Python \n")
+#Pass multiple arguments for printing
+print("hello",5)
+#End argument of Print Statement
+print("welcome to", end= "*")
