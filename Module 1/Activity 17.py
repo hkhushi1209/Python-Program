aebffa2a9cb28c4fd2b4b4887d5f8ea49b265223
hkhushi1209 <hkhushi1209@gmail.com>
2025-05-13 17:31:20 +0530

    Commit 17

---
+++ Module 1/Activity 17.py
@@ -0,0 +1,8 @@
+print("Number is positive, negative or Zero")
+number = int(input("Enter Number: "))
+if number > 0:
+    print(number, "is Positive")
+elif number < 0:
+    print(number, "is Negative")
+else:
+    print(number, "is Zero")
