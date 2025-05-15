bd096519c9b1e95d1e77a259ff53c40f78f788e7
hkhushi1209 <hkhushi1209@gmail.com>
2025-05-15 17:26:35 +0530

    Commit 23

---
+++ Module 1/Activity 23.py
@@ -0,0 +1,16 @@
+i = 13
+if (i == 13):
+    if (i < 15):
+        print("i is smaller than 15")
+    if (i < 13):
+        print("i is smaller than 13 too")
+    else:
+        print("i is greater than 12 and smaller than 15")
+        n = int(input("enter a number: "))
+if n >= 0:
+    if n == 0:
+        print("number is zero")
+    else:
+        print("number is positive")
+else:
+    print("number is negative")
