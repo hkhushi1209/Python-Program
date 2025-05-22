dc878f8c8b4f0c096c4163d487c09cb22da976d9
hkhushi1209 <hkhushi1209@gmail.com>
2025-05-22 17:32:25 +0530

    Commit 31

---
+++ Module 1/Activity 31.py
@@ -0,0 +1,9 @@
+print("while loop")
+n=int(input("enter number: ")) # initialize variable
+while(n>=0): # condition
+    print(n)
+    n-=2 #increment / decreament variable
+    i=0  # initialize variable
+while(i<=5): # condition
+    print(i)
+    i+=1  #increament
