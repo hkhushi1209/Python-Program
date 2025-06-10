1e3ad02dc178c4dc1c296098bc62a2368b03ecd3
hkhushi1209 <hkhushi1209@gmail.com>
2025-06-10 17:28:04 +0530

    Commit 48

--- Module 1/Activity 48.py
+++ Module 1/Activity 48.py
@@ -2,3 +2,9 @@ def my_function():
   print("Hello from a function")
 
 my_function()
+
+def my_function(name,place):
+  print(name,place)
+
+my_function("kirsh", "Paris")
+my_function("raj", "mumbai")
---
+++ Module 1/Activity 49.py
@@ -0,0 +1,4 @@
+def my_function(name):
+  return name
+
+print(my_function("kirsh"))
