acc99081feb925f565a961d9466153b7570600db
hkhushi1209 <hkhushi1209@gmail.com>
2025-06-17 17:26:36 +0530

    Commit 59

--- Module 1/Activity 59.py
+++ Module 1/Activity 59.py
@@ -1,8 +1,7 @@
+#using a try and except
 try:
-    age = int(input("Enter the age:"))
-    if(age < 18):
-        raise ValueError
-    else:
-        print("the age is valid")
-except ValueError:
-    print("The age is not valid")
+    number = int(input("Enter a number: "))
+    print("The number entered is", number)
+#using value error
+except ValueError as ex:
+    print("Exception:", ex)
