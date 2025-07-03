d65b0dca272a48a44c489910ac51abc3330b00e0
hkhushi1209 <hkhushi1209@gmail.com>
2025-07-03 16:42:50 +0530

    Commit 64

---
+++ Data Structure/Activity 4.py
@@ -0,0 +1,10 @@
+# create a tuple using ()
+# number tuple
+number_tuple = (10, 20, 25.75)
+print(number_tuple)
+# Output (10, 20, 25.75)
+
+# string tuple
+string_tuple = ('Jessa', 'Emma', 'Kelly')
+print(string_tuple)
+# Output ('Jessa', 'Emma', 'Kelly')
---
+++ Data Structure/Activity 5.py
@@ -0,0 +1,4 @@
+tuple1 = ('P', 'Y', 'T', 'H', 'O', 'N')
+# length of a tuple
+print(len(tuple1))
+# Output 6
