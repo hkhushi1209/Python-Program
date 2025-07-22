c49fe4524bdd44f142db10a8b97b19d8c444cc6b
hkhushi1209 <hkhushi1209@gmail.com>
2025-07-22 18:01:43 +0530

    Commit 67

---
+++ Data Structure/Activity 24.py
@@ -0,0 +1,5 @@
+class fruit:
+    print("hey! this is an example of classes and objects")
+
+# Object Creation
+ob = fruit()
---
+++ Data Structure/Activity 25.py
@@ -0,0 +1,10 @@
+class fruit:
+    # Constructor
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+# Object Creation
+apple = fruit('Apple', 'Red')
+# Calling Function
+print(apple.name)
